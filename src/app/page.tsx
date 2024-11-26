7fc734fb733329c593f01b0200465d443c3bcaa1
Catalin Dumitru <dumitru.catalin96@yahoo.com>
2024-11-26 09:53:38 +0200

    Delete unnecessary render check.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import LSEGChatbot from "@/components/LSEGChatbot";
 
 export default function Home() {
-  const [isRendered, setIsRendered] = useState(false);
-
-  useEffect(() => {
-    setIsRendered(true);
-  }, []);
-
   return (
-    isRendered && (
-      <div className="w-full h-screen flex flex-col">
-        <main className="flex-1 flex flex-col items-center justify-center">
-          <LSEGChatbot />
-        </main>
-      </div>
-    )
+    <div className="w-full h-screen flex flex-col">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <LSEGChatbot />
+      </main>
+    </div>
   );
 }
